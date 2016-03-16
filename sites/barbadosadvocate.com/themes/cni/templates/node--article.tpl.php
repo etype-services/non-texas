883e41aeb0daa0a69a7920a364b5834d709f2073
Charlie Llewellin <charliellewellin@gmail.com>
2016-03-16 16:33:40 -0500

    remove site master themes and fix barbados more link

--- sites/barbadosadvocate.com/themes/cni/templates/node--article.tpl.php
+++ sites/barbadosadvocate.com/themes/cni/templates/node--article.tpl.php
@@ -46,7 +46,7 @@
       ?>
     </div>
     
-    <div class="view-more-digital"><a href="http://www.etypeservices.com/Fernandina%20Beach%20News-LeaderID565/">To view more please log in or subscribe to the digital edition.</a></div>
+    <div class="view-more-digital"><a href="http://www.etypeservices.com/Barbados%20AdvocateID432/">To view more please log in or subscribe to the digital edition.</a></div>
     
     <?php if (isset($content['links'])): ?>  
       <?php print render($content['links']); ?>
