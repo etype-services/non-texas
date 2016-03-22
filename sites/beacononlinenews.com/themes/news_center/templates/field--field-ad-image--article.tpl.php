77dbe943215c68e053b04d3606378c398ff4e9b2
Charlie Llewellin <charliellewellin@gmail.com>
2016-03-22 10:21:53 -0500

    volusia

--- sites/beacononlinenews.com/themes/news_center/templates/field--field-ad-image--article.tpl.php
+++ sites/beacononlinenews.com/themes/news_center/templates/field--field-ad-image--article.tpl.php
@@ -1,8 +1,8 @@
 <?php
-var_dump($items);
+// var_dump($items);
 // $field = field_get_items('node', $node, 'field_ad_image');
-//$output = field_view_value('node', $node, 'field_ad_image', $image[0]);
+// $output = field_view_value('node', $node, 'field_ad_image', $image[0]);
 ?>
 <div class="field_ad_image">
-  <a href=""><img src="<?php print file_create_url($items[0]['#item']['uri']); ?>" /></a>
+  <a href="<?php print render($content['field_ad_url']) ?>"><img src="<?php print file_create_url($items[0]['#item']['uri']); ?>" /></a>
 </div>
