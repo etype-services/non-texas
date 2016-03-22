c1445d52ca9ae0b1247c1dc8ddf9248afd1f5514
Charlie Llewellin <charliellewellin@gmail.com>
2016-03-22 10:25:38 -0500

    volusia

--- sites/beacononlinenews.com/themes/news_center/templates/page.tpl.php
+++ sites/beacononlinenews.com/themes/news_center/templates/page.tpl.php
@@ -63,7 +63,7 @@
  * - $page['sidebar_first']: Items for the first sidebar.
  * - $page['sidebar_second']: Items for the second sidebar.
  * - $page['header']: Items for the header region.
- * - $page['top_area']: Items for the Top Area region. 
+ * - $page['top_area']: Items for the Top Area region.
  * - $page['footer']: Items for the footer region.
  * - $page['footer_first_column']: Items for the footer first column region.
  * - $page['footer_second_column']: Items for the footer second column region.
@@ -79,98 +79,119 @@
  */
 ?>
 
-<div id="page-wrapper" class="container" ><div id="page">
-		
-		<?php print render($page['top_advertisement']); ?>  
-	
-  <div id="header"><div class="section clearfix">
+<div id="page-wrapper" class="container">
+  <div id="page">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-	  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="mobile-logo"><img src="<?php print $base_path; ?><?php print $directory; ?>/logo_mobile.png" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
+    <?php print render($page['top_advertisement']); ?>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"><strong>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </strong></div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
+    <div id="header">
+      <div class="section clearfix">
+
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>"
+             title="<?php print t('Home'); ?>" rel="home" id="logo"><img
+              src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/></a>
+          <a href="<?php print $front_page; ?>"
+             title="<?php print t('Home'); ?>" rel="home" id="mobile-logo"><img
+              src="<?php print $base_path; ?><?php print $directory; ?>/logo_mobile.png"
+              alt="<?php print t('Home'); ?>"/></a>
         <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php if ($site_name || $site_slogan): ?>
+          <div id="name-and-slogan">
+            <?php if ($site_name): ?>
+              <?php if ($title): ?>
+                <div id="site-name"><strong>
+                    <a href="<?php print $front_page; ?>"
+                       title="<?php print t('Home'); ?>"
+                       rel="home"><span><?php print $site_name; ?></span></a>
+                  </strong></div>
+              <?php else: /* Use h1 when the content title is empty */ ?>
+                <h1 id="site-name">
+                  <a href="<?php print $front_page; ?>"
+                     title="<?php print t('Home'); ?>"
+                     rel="home"><span><?php print $site_name; ?></span></a>
+                </h1>
+              <?php endif; ?>
+            <?php endif; ?>
+
+            <?php if ($site_slogan): ?>
+              <div id="site-slogan"><?php print $site_slogan; ?></div>
+            <?php endif; ?>
+          </div><!-- /#name-and-slogan -->
         <?php endif; ?>
-      </div><!-- /#name-and-slogan -->
-    <?php endif; ?>
-	
-	<?php print render($page['top_area']); ?>    
 
-    <?php print render($page['header']); ?>
+        <?php print render($page['top_area']); ?>
 
-  </div></div><!-- /.section, /#header -->
+        <?php print render($page['header']); ?>
 
-  <div id="main-wrapper"><div id="main" class="clearfix<?php if ($main_menu || $page['navigation']) { print ' with-navigation'; } ?>">
+      </div>
+    </div><!-- /.section, /#header -->
 
-    <div id="content" class="column"><div class="section">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-	  
-	  <?php print render($page['slideshow']); ?>
-	  
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if ($tabs = render($tabs)): ?>
-        <div class="tabs"><?php print $tabs; ?></div>
-      <?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-	  <?php print render($page['content0']); ?>
-	  <?php print render($page['content1']); ?>
-	  <?php print render($page['content2']); ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div></div><!-- /.section, /#content -->   
+    <div id="main-wrapper">
+      <div id="main"
+           class="clearfix<?php if ($main_menu || $page['navigation']) {
+             print ' with-navigation';
+           } ?>">
 
-    <?php print render($page['sidebar_first']); ?>
+        <div id="content" class="column">
+          <div class="section">
+            <?php print render($page['highlighted']); ?>
+            <?php print $breadcrumb; ?>
 
-    <?php print render($page['sidebar_second']); ?>
+            <?php print render($page['slideshow']); ?>
 
-  </div></div><!-- /#main, /#main-wrapper -->
-  </div></div><!-- /#page, /#page-wrapper -->
-  <div id="footer">
-  <div class="footer-inner">
-  
-    <?php print render($page['footer']); ?>
-  
-    <?php print render($page['footer_first_column']); ?>
-	
-	<?php print render($page['footer_second_column']); ?>
-	
-	<?php print render($page['footer_third_column']); ?>
-	
-	<?php print render($page['footer_fourth_column']); ?>
-	
-	<div class="clear"></div>
-	
-	
-	</div>
-	<?php print render($page['footer_closure']); ?>
+            <a id="main-content"></a>
+
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+              <h1 class="title" id="page-title"><?php print $title; ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            <?php print $messages; ?>
+            <?php if ($tabs = render($tabs)): ?>
+              <div class="tabs"><?php print $tabs; ?></div>
+            <?php endif; ?>
+            <?php print render($page['help']); ?>
+            <?php if ($action_links): ?>
+              <ul
+                class="action-links"><?php print render($action_links); ?></ul>
+            <?php endif; ?>
+            <?php print render($page['content0']); ?>
+            <?php print render($page['content1']); ?>
+            <?php print render($page['content2']); ?>
+            <?php print render($page['content']); ?>
+            <?php print $feed_icons; ?>
+          </div>
+        </div><!-- /.section, /#content -->
+
+        <?php print render($page['sidebar_first']); ?>
+
+        <?php print render($page['sidebar_second']); ?>
+
+      </div>
+    </div><!-- /#main, /#main-wrapper -->
   </div>
+</div><!-- /#page, /#page-wrapper -->
+<div id="footer">
+  <div class="footer-inner">
 
+    <?php print render($page['footer']); ?>
+
+    <?php print render($page['footer_first_column']); ?>
+
+    <?php print render($page['footer_second_column']); ?>
+
+    <?php print render($page['footer_third_column']); ?>
+
+    <?php print render($page['footer_fourth_column']); ?>
+
+    <div class="clear"></div>
+
+
+  </div>
+  <?php print render($page['footer_closure']); ?>
+</div>
 
 
 <?php print render($page['bottom']); ?>
