f02a050e02eb8183f2ae0938ec8c5cdae6bcc27a
Charlie Llewellin <charliellewellin@gmail.com>
2016-03-22 16:24:44 -0500

    volusia

--- sites/test.etypegoogle8.com/themes/news_center/template.php
+++ sites/test.etypegoogle8.com/themes/news_center/template.php
@@ -87,16 +87,17 @@ function STARTERKIT_preprocess_html(&$variables, $hook) {
  */
 
 function news_center_preprocess_page(&$variables, $hook) {
+  $variables['node_ad'] = '';
   if (isset($variables['node'])) {
     $node = $variables['node'];
-  	$arr = array();
-  	$ad = field_get_items('node', $node, 'field_ad_image');
-  	$arr['img_src'] = file_create_url($ad[0]['uri']);
-  	$url = field_get_items('node', $variables['node'], 'field_ad_url');
-  	$arr['img_url'] = $url[0]['safe_value'];
-  	$variables['node_ad'] = theme_render_template('sites/test.etypegoogle8.com/themes/news_center/templates/field--field-ad-image--article.tpl.php', $arr);
-  } else {
-  	$variables['node_ad'] = '';
+    $ad = field_get_items('node', $node, 'field_ad_image');
+    if (!empty($ad[0]['uri'])) {
+      $arr = array();
+      $arr['img_src'] = file_create_url($ad[0]['uri']);
+      $url = field_get_items('node', $variables['node'], 'field_ad_url');
+      $arr['img_url'] = $url[0]['safe_value'];
+      $variables['node_ad'] = theme_render_template('sites/test.etypegoogle8.com/themes/news_center/templates/field--field-ad-image--article.tpl.php', $arr);
+    }
   }
 }
 
