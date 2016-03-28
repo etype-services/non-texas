2aad46493195b7ac8ebdc940c5f72b32abd8078d
Charlie Llewellin <charliellewellin@gmail.com>
2016-03-28 16:20:30 -0500

    volusia beacon ad

--- sites/beacononlinenews.com/themes/news_center/templates/field--field-ad-image--article.tpl.php
+++ sites/beacononlinenews.com/themes/news_center/templates/field--field-ad-image--article.tpl.php
@@ -1,5 +1,5 @@
 <div class="field_ad_image">
+  <p>This story brought to you for free by:</p>
   <div class="field_ad_image_inner">
-    <p>This story brought to you for free by:</p>
     <a href="<?php print $img_url ?>"><img src="<?php print $img_src; ?>" /></a></div>
 </div>
