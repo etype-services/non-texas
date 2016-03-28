3f4cc39b4f4688f8cbb81313d0ec4a215cf88e63
Charlie Llewellin <charliellewellin@gmail.com>
2016-03-28 16:00:59 -0500

    volusia beacon ad

--- sites/beacononlinenews.com/themes/news_center/templates/field--field-ad-image--article.tpl.php
+++ sites/beacononlinenews.com/themes/news_center/templates/field--field-ad-image--article.tpl.php
@@ -4,5 +4,6 @@
 // $output = field_view_value('node', $node, 'field_ad_image', $image[0]);
 ?>
 <div class="field_ad_image">
+  <p>This story brought to you for free by:</p>
   <div class="field_ad_image_inner"><a href="<?php print $img_url ?>"><img src="<?php print $img_src; ?>" /></a></div>
 </div>
