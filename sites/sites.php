90d366ec5f08e2c099a70858f3f179209f38afdf
Charlie Llewellin <charliellewellin@gmail.com>
2016-05-16 13:51:27 -0500

    kc globe

--- sites/sites.php
+++ sites/sites.php
@@ -3,106 +3,70 @@
 $sites['prairiepress.etypegoogle8.com'] = 'prairiepress.net';
 $sites['prairiepress.net'] = 'prairiepress.net';
 $sites['www.prairiepress.net'] = 'prairiepress.net';
-$sites['prod.prairiepress.net'] = 'prairiepress.net';
-$sites['dev.prairiepress.net'] = 'prairiepress.net';
 
 $sites['theprairiepress.net'] = 'prairiepress.net';
 $sites['www.theprairiepress.net'] = 'prairiepress.net';
-$sites['prod.theprairiepress.net'] = 'prairiepress.net';
-$sites['dev.theprairiepress.net'] = 'prairiepress.net';
 
 $sites['backroadsnews.etypegoogle8.com'] = 'backroadsnews.com';
 $sites['backroadsnews.com'] = 'backroadsnews.com';
 $sites['www.backroadsnews.com'] = 'backroadsnews.com';
-$sites['prod.backroadsnews.com'] = 'backroadsnews.com';
-$sites['dev.backroadsnews.com'] = 'backroadsnews.com';
 
 $sites['theclaycountynews.etypegoogle8.com'] = 'theclaycountynews.com';
 $sites['theclaycountynews.com'] = 'theclaycountynews.com';
 $sites['www.theclaycountynews.com'] = 'theclaycountynews.com';
-$sites['prod.theclaycountynews.com'] = 'theclaycountynews.com';
-$sites['dev.theclaycountynews.com'] = 'theclaycountynews.com';
 
 /* gunnisontimes.etypegoogle8.com */
 $sites['gunnisontimes.etypegoogle8.com'] = 'gunnisontimes.com';
 $sites['gunnisontimes.com'] = 'gunnisontimes.com';
 $sites['www.gunnisontimes.com'] = 'gunnisontimes.com';
-$sites['prod.gunnisontimes.com'] = 'gunnisontimes.com';
-$sites['dev.gunnisontimes.com'] = 'gunnisontimes.com';
 $sites['photo.gunnisontimes.com'] = 'gunnisontimes.com';
 
 $sites['gunnisoncountrytimes.com'] = 'gunnisontimes.com';
 $sites['www.gunnisoncountrytimes.com'] = 'gunnisontimes.com';
-$sites['prod.gunnisoncountrytimes.com'] = 'gunnisontimes.com';
-$sites['dev.gunnisoncountrytimes.com'] = 'gunnisontimes.com';
 
 /* ouraynews.etypegoogle8.com */
 $sites['ouraynews.etypegoogle8.com'] = 'ouraynews.com';
 $sites['ouraynews.com'] = 'ouraynews.com';
 $sites['www.ouraynews.com'] = 'ouraynews.com';
-$sites['prod.ouraynews.com'] = 'ouraynews.com';
-$sites['dev.ouraynews.com'] = 'ouraynews.com';
 
 $sites['ouraycoloradonews.com'] = 'ouraynews.com';
 $sites['www.ouraycoloradonews.com'] = 'ouraynews.com';
-$sites['prod.ouraycoloradonews.com'] = 'ouraynews.com';
-$sites['dev.ouraycoloradonews.com'] = 'ouraynews.com';
 
 $sites['ouraycountyplaindealer.com'] = 'ouraynews.com';
 $sites['www.ouraycountyplaindealer.com'] = 'ouraynews.com';
-$sites['prod.ouraycountyplaindealer.com'] = 'ouraynews.com';
-$sites['dev.ouraycountyplaindealer.com'] = 'ouraynews.com';
 
 $sites['ourayplaindealer.com'] = 'ouraynews.com';
 $sites['www.ourayplaindealer.com'] = 'ouraynews.com';
-$sites['prod.ourayplaindealer.com'] = 'ouraynews.com';
-$sites['dev.ourayplaindealer.com'] = 'ouraynews.com';
 
 $sites['ridgewaycoloradonews.com'] = 'ouraynews.com';
 $sites['www.ridgewaycoloradonews.com'] = 'ouraynews.com';
-$sites['prod.ridgewaycoloradonews.com'] = 'ouraynews.com';
-$sites['dev.ridgewaycoloradonews.com'] = 'ouraynews.com';
 
 $sites['ridgewaynews.com'] = 'ouraynews.com';
 $sites['www.ridgewaynews.com'] = 'ouraynews.com';
-$sites['prod.ridgewaynews.com'] = 'ouraynews.com';
-$sites['dev.ridgewaynews.com'] = 'ouraynews.com';
 
 $sites['ridgewaysun.com'] = 'ouraynews.com';
 $sites['www.ridgewaysun.com'] = 'ouraynews.com';
-$sites['prod.ridgewaysun.com'] = 'ouraynews.com';
-$sites['dev.ridgewaysun.com'] = 'ouraynews.com';
 
 $sites['theridgewaysun.com'] = 'ouraynews.com';
 $sites['www.theridgewaysun.com'] = 'ouraynews.com';
-$sites['prod.theridgewaysun.com'] = 'ouraynews.com';
-$sites['dev.theridgewaysun.com'] = 'ouraynews.com';
 
 /* barbadosadvocate.etypegoogle8.com */
 $sites['barbadosadvocate.etypegoogle8.com'] = 'barbadosadvocate.com';
 $sites['barbadosadvocate.com'] = 'barbadosadvocate.com';
 $sites['www.barbadosadvocate.com'] = 'barbadosadvocate.com';
-$sites['prod.barbadosadvocate.com'] = 'barbadosadvocate.com';
-$sites['dev.barbadosadvocate.com'] = 'barbadosadvocate.com';
 
 $sites['henryettafree-lance.etypegoogle8.com'] = 'henryettafree-lance.com';
 $sites['henryettafree-lance.com'] = 'henryettafree-lance.com';
 $sites['www.henryettafree-lance.com'] = 'henryettafree-lance.com';
-$sites['prod.henryettafree-lance.com'] = 'henryettafree-lance.com';
-$sites['dev.henryettafree-lance.com'] = 'henryettafree-lance.com';
 
 $sites['yourokmulgee.etypegoogle8.com'] = 'yourokmulgee.com';
 $sites['yourokmulgee.com'] = 'yourokmulgee.com';
 $sites['www.yourokmulgee.com'] = 'yourokmulgee.com';
-$sites['prod.yourokmulgee.com'] = 'yourokmulgee.com';
-$sites['dev.yourokmulgee.com'] = 'yourokmulgee.com';
 
 /* lutchernews.etypegoogle8.com */
 $sites['lutchernews.etypegoogle8.com'] = 'lutchernews.com';
 $sites['lutchernews.com'] = 'lutchernews.com';
 $sites['www.lutchernews.com'] = 'lutchernews.com';
-$sites['prod.lutchernews.com'] = 'lutchernews.com';
-$sites['dev.lutchernews.com'] = 'lutchernews.com';
 
 $sites['stjamesparishtoday.com'] = 'lutchernews.com';
 $sites['www.stjamesparishtoday.com'] = 'lutchernews.com';
@@ -114,52 +78,36 @@ $sites['www.vacherienews.com'] = 'lutchernews.com';
 $sites['tecumsehchieftain.etypegoogle8.com'] = 'tecumsehchieftain.com';
 $sites['tecumsehchieftain.com'] = 'tecumsehchieftain.com';
 $sites['www.tecumsehchieftain.com'] = 'tecumsehchieftain.com';
-$sites['prod.tecumsehchieftain.com'] = 'tecumsehchieftain.com';
-$sites['dev.tecumsehchieftain.com'] = 'tecumsehchieftain.com';
 
 /* pawneerepublican.etypegoogle8.com */ 
 $sites['pawneerepublican.etypegoogle8.com'] = 'pawneerepublican.com';
 $sites['pawneerepublican.com'] = 'pawneerepublican.com';
 $sites['www.pawneerepublican.com'] = 'pawneerepublican.com';
-$sites['prod.pawneerepublican.com'] = 'pawneerepublican.com';
-$sites['dev.pawneerepublican.com'] = 'pawneerepublican.com';
 
 $sites['pawneenews.com'] = 'pawneerepublican.com';
 $sites['www.pawneenews.com'] = 'pawneerepublican.com';
-$sites['prod.pawneenews.com'] = 'pawneerepublican.com';
-$sites['dev.pawneenews.com'] = 'pawneerepublican.com';
 
 /* beacononlinenews.etypegoogle8.com */
 $sites['beacononlinenews.etypegoogle8.com'] = 'beacononlinenews.com';
 $sites['beacononlinenews.com'] = 'beacononlinenews.com';
 $sites['www.beacononlinenews.com'] = 'beacononlinenews.com';
-$sites['prod.beacononlinenews.com'] = 'beacononlinenews.com';
-$sites['dev.beacononlinenews.com'] = 'beacononlinenews.com';
 
 $sites['indyrepnews.etypegoogle8.com'] = 'indyrepnews.com';
 $sites['indyrepnews.com'] = 'indyrepnews.com';
 $sites['www.indyrepnews.com'] = 'indyrepnews.com';
-$sites['prod.indyrepnews.com'] = 'indyrepnews.com';
-$sites['dev.indyrepnews.com'] = 'indyrepnews.com';
 
 $sites['msgr.etypegoogle8.com'] = 'msgr.com';
 $sites['msgr.com'] = 'msgr.com';
 $sites['www.msgr.com'] = 'msgr.com';
-$sites['prod.msgr.com'] = 'msgr.com';
-$sites['dev.msgr.com'] = 'msgr.com';
 
 $sites['myironcountynews.etypegoogle8.com'] = 'myironcountynews.com';
 $sites['myironcountynews.com'] = 'myironcountynews.com';
 $sites['www.myironcountynews.com'] = 'myironcountynews.com';
-$sites['prod.myironcountynews.com'] = 'myironcountynews.com';
-$sites['dev.myironcountynews.com'] = 'myironcountynews.com';
 
 /* fairburyjournalnews.etypegoogle8.com */
 $sites['fairburyjournalnews.etypegoogle8.com'] = 'fairburyjournalnews.com';
 $sites['fairburyjournalnews.com'] = 'fairburyjournalnews.com';
 $sites['www.fairburyjournalnews.com'] = 'fairburyjournalnews.com';
-$sites['prod.fairburyjournalnews.com'] = 'fairburyjournalnews.com';
-$sites['dev.fairburyjournalnews.com'] = 'fairburyjournalnews.com';
 
 $sites['thefairburyjournal.com'] = 'fairburyjournalnews.com';
 $sites['www.thefairburyjournal.com'] = 'fairburyjournalnews.com';
@@ -168,45 +116,31 @@ $sites['www.thefairburyjournal.com'] = 'fairburyjournalnews.com';
 $sites['westessextribune.etypegoogle8.com'] = 'westessextribune.net';
 $sites['westessextribune.net'] = 'westessextribune.net';
 $sites['www.westessextribune.net'] = 'westessextribune.net';
-$sites['prod.westessextribune.net'] = 'westessextribune.net';
-$sites['dev.westessextribune.net'] = 'westessextribune.net';
 
 $sites['jcnews.etypegoogle8.com'] = 'jcnews.com';
 $sites['jcnews.com'] = 'jcnews.com';
 $sites['www.jcnews.com'] = 'jcnews.com';
-$sites['prod.jcnews.com'] = 'jcnews.com';
-$sites['dev.jcnews.com'] = 'jcnews.com';
 
 $sites['mysanfordherald.etypegoogle8.com'] = 'mysanfordherald.com';
 $sites['mysanfordherald.com'] = 'mysanfordherald.com';
 $sites['www.mysanfordherald.com'] = 'mysanfordherald.com';
-$sites['prod.mysanfordherald.com'] = 'mysanfordherald.com';
-$sites['dev.mysanfordherald.com'] = 'mysanfordherald.com';
 
 $sites['lakeoconeenews.etypegoogle8.com'] = 'lakeoconeenews.us';
 $sites['lakeoconeenews.us'] = 'lakeoconeenews.us';
 $sites['www.lakeoconeenews.us'] = 'lakeoconeenews.us';
-$sites['prod.lakeoconeenews.us'] = 'lakeoconeenews.us';
-$sites['dev.lakeoconeenews.us'] = 'lakeoconeenews.us';
 
 $sites['plaqueminesgazette.etypegoogle8.com'] = 'plaqueminesgazette.com';
 $sites['plaqueminesgazette.com'] = 'plaqueminesgazette.com';
 $sites['www.plaqueminesgazette.com'] = 'plaqueminesgazette.com';
-$sites['prod.plaqueminesgazette.com'] = 'plaqueminesgazette.com';
-$sites['dev.plaqueminesgazette.com'] = 'plaqueminesgazette.com';
 
 $sites['thestbernardvoice.etypegoogle8.com'] = 'thestbernardvoice.com';
 $sites['thestbernardvoice.com'] = 'thestbernardvoice.com';
 $sites['www.thestbernardvoice.com'] = 'thestbernardvoice.com';
-$sites['prod.thestbernardvoice.com'] = 'thestbernardvoice.com';
-$sites['dev.thestbernardvoice.com'] = 'thestbernardvoice.com';
 
 /* timberlakesouthdakota.etypegoogle8.com */
 $sites['timberlakesouthdakota.etypegoogle8.com'] = 'timberlakesouthdakota.com';
 $sites['timberlakesouthdakota.com'] = 'timberlakesouthdakota.com';
 $sites['www.timberlakesouthdakota.com'] = 'timberlakesouthdakota.com';
-$sites['prod.timberlakesouthdakota.com'] = 'timberlakesouthdakota.com';
-$sites['dev.timberlakesouthdakota.com'] = 'timberlakesouthdakota.com';
 
 $sites['timberlaketopic.com'] = 'timberlakesouthdakota.com';
 $sites['www.timberlaketopic.com'] = 'timberlakesouthdakota.com';
@@ -215,11 +149,20 @@ $sites['www.timberlaketopic.com'] = 'timberlakesouthdakota.com';
 $sites['thekansascityglobe.etypegoogle8.com'] = 'thekansascityglobe.com';
 $sites['thekansascityglobe.com'] = 'thekansascityglobe.com';
 $sites['www.thekansascityglobe.com'] = 'thekansascityglobe.com';
-$sites['prod.thekansascityglobe.com'] = 'thekansascityglobe.com';
-$sites['dev.thekansascityglobe.com'] = 'thekansascityglobe.com';
 
+$sites['thekansascityglobe.net'] = 'thekansascityglobe.com';
+$sites['www.thekansascityglobe.net'] = 'thekansascityglobe.com';
+
+$sites['thekansascityglobe.info'] = 'thekansascityglobe.com';
+$sites['www.thekansascityglobe.info'] = 'thekansascityglobe.com';
+
+$sites['thekansascityglobe.org'] = 'thekansascityglobe.com';
+$sites['www.thekansascityglobe.org'] = 'thekansascityglobe.com';
+
+$sites['thekcglobe.com'] = 'thekansascityglobe.com';
+$sites['www.thekcglobe.com'] = 'thekansascityglobe.com';
+
+/* auroranewsregister.etypegoogle8.com */
 $sites['auroranewsregister.etypegoogle8.com'] = 'auroranewsregister.com';
 $sites['auroranewsregister.com'] = 'auroranewsregister.com';
 $sites['www.auroranewsregister.com'] = 'auroranewsregister.com';
-$sites['prod.auroranewsregister.com'] = 'auroranewsregister.com';
-$sites['dev.auroranewsregister.com'] = 'auroranewsregister.com';
