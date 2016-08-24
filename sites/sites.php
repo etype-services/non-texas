2e541bb3a3494ca957a1733cc88f5f4b3678b015
Charlie Llewellin <charlie@llewellin.net>
2016-08-24 15:31:33 +0100

    newsblade.com

--- sites/sites.php
+++ sites/sites.php
@@ -202,3 +202,8 @@ $sites['www.thejenatimes.net'] = 'thejenatimes.net';
 $sites['stiglernews.etypegoogle8.com'] = 'stiglernews.com';
 $sites['stiglernews.com'] = 'stiglernews.com';
 $sites['www.stiglernews.com'] = 'stiglernews.com';
+
+/* newsblade.etypegoogle8.com */
+$sites['newsblade.etypegoogle8.com'] = 'newsblade.com';
+$sites['newsblade.com'] = 'newsblade.com';
+$sites['www.newsblade.com'] = 'newsblade.com';
