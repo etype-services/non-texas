b43ee92a70adea6fee6c6861ee7c765f621411ac
Charlie Llewellin <charlie@llewellin.net>
2017-01-30 11:24:04 -0600

    fred 22 theme

--- sites/all/themes/fredtwentytwo/theme-settings.php
+++ sites/all/themes/fredtwentytwo/theme-settings.php
@@ -7,21 +7,27 @@
  * @param $form_state
  *   A keyed array containing the current state of the form.
  */
-function fredtwentytwo_form_system_theme_settings_alter(&$form, &$form_state)  {
+function fredtwentytwo_form_system_theme_settings_alter(&$form, &$form_state) {
 
-  // Create the form using Forms API: http://api.drupal.org/api/7
+    // Create the form using Forms API: http://api.drupal.org/api/7
+
+    $form['nav_color'] = array(
+        '#type' => 'textfield',
+        '#title' => t('Navigation Color'),
+        '#size' => 20,
+        '#default_value' => theme_get_setting('nav_color'),
+    );
+
+    $form['body_background'] = array(
+        '#type' => 'textfield',
+        '#title' => t('Body Background'),
+        '#size' => 20,
+        '#default_value' => theme_get_setting('body_background'),
+    );
 
 
-  $form['STARTERKIT_example'] = array(
-    '#type'          => 'checkbox',
-    '#title'         => t('STARTERKIT sample setting'),
-    '#default_value' => theme_get_setting('STARTERKIT_example'),
-    '#description'   => t("This option doesn't do anything; it's just an example."),
-  );
+    // Remove some of the base theme's settings.
+    unset($form['themedev']['zen_layout']); // We don't need to select the layout stylesheet.
 
-
-  // Remove some of the base theme's settings.
-  unset($form['themedev']['zen_layout']); // We don't need to select the layout stylesheet.
-
-  // We are editing the $form in place, so we don't need to return anything.
+    // We are editing the $form in place, so we don't need to return anything.
 }
