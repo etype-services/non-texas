b02c0a96168ffc6265643b9351e213109316ab81
Charlie Llewellin <charlie@llewellin.net>
2017-02-01 10:52:04 -0600

    news_center theme

--- sites/msgr.com/themes/msgr/templates/html.tpl.php
+++ sites/msgr.com/themes/msgr/templates/html.tpl.php
@@ -62,21 +62,17 @@
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
         "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"
-      xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0"
-      dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
-
+      xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 <head profile="<?php print $grddl_profile; ?>">
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1, user-scalable=no"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1, user-scalable=no"/>
     <?php print $styles; ?>
     <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
 <div id="skip-link">
-    <a href="#<?php print $jump_link_target; ?>"
-       class="element-invisible element-focusable"><?php print t('Jump to Navigation'); ?></a>
+    <a href="#<?php print $jump_link_target; ?>" class="element-invisible element-focusable"><?php print t('Jump to Navigation'); ?></a>
 </div>
 <?php print $page_top; ?>
 <?php print $page; ?>
