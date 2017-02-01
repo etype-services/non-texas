f00f937c90f3925640010650f0c1800cfe5326f8
etypeweb@non-texas.c.modern-photon-112115.internal <etypeweb@non-texas.c.modern-photon-112115.internal>
2017-02-01 10:50:03 -0600

    msgr logo

--- sites/msgr.com/themes/msgr/templates/page.tpl.php
+++ sites/msgr.com/themes/msgr/templates/page.tpl.php
@@ -92,11 +92,11 @@
                        title="<?php print t('Home'); ?>" rel="home"
                        id="logo"><img src="<?php print $logo; ?>"
                                       alt="<?php print t('Home'); ?>"/></a>
-                    <a href="<?php print $front_page; ?>"
+                   <!-- <a href="<?php print $front_page; ?>"
                        title="<?php print t('Home'); ?>" rel="home"
                        id="mobile-logo"><img
                                 src="<?php print $base_path; ?><?php print $directory; ?>/logo_mobile.png"
-                                alt="<?php print t('Home'); ?>"/></a>
+                                alt="<?php print t('Home'); ?>"/></a> -->
                 <?php endif; ?>
 
                 <?php if ($site_name || $site_slogan): ?>
