846dd75e456dd9accf663471d0a82dd059669f1b
Charlie Llewellin <charlie@llewellin.net>
2017-02-01 20:50:30 -0600

    news_center theme

--- sites/all/themes/news_center/templates/views-view-fields--home-page--block.tpl.php
+++ sites/all/themes/news_center/templates/views-view-fields--home-page--block.tpl.php
@@ -31,24 +31,19 @@
 	<?php print $fields['field_image']->content; ?>
   <?php print $fields['field_image']->wrapper_suffix; ?>
 
-<div class="slideshow-overlay" >
+<?php foreach ($fields as $id => $field): ?>
 
-	
-	<?php foreach ($fields as $id => $field): ?>
-	
-	  <?php if ($field->content != $fields['field_image']->content): ?>
-	  
-		  <?php if (!empty($field->separator)): ?>
-			<?php print $field->separator; ?>
-		  <?php endif; ?>
+    <?php if ($field->content != $fields['field_image']->content): ?>
 
-		  <?php print $field->wrapper_prefix; ?>
-			<?php print $field->label_html; ?>
-			<?php print $field->content; ?>
-		  <?php print $field->wrapper_suffix; ?>
-		  
-	  <?php endif; ?>
-	  
-	<?php endforeach; ?>
+        <?php if (!empty($field->separator)): ?>
+            <?php print $field->separator; ?>
+        <?php endif; ?>
 
-</div>
+        <?php print $field->wrapper_prefix; ?>
+        <?php print $field->label_html; ?>
+        <?php print $field->content; ?>
+        <?php print $field->wrapper_suffix; ?>
+
+    <?php endif; ?>
+
+<?php endforeach; ?>
