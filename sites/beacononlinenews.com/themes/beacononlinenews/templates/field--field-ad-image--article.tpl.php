8ffa2bb7b2e772ea05d2acc575a3d12277d1fc22
etypeweb@non-texas.c.modern-photon-112115.internal <etypeweb@non-texas.c.modern-photon-112115.internal>
2017-02-02 14:41:19 -0600

    msgr page.tpl

--- sites/beacononlinenews.com/themes/beacononlinenews/templates/field--field-ad-image--article.tpl.php
+++ sites/beacononlinenews.com/themes/beacononlinenews/templates/field--field-ad-image--article.tpl.php
@@ -1,5 +1,5 @@
 <div class="field_ad_image">
   <p>This story brought to you for free by:</p>
   <div class="field_ad_image_inner">
-    <a href="<?php print $img_url ?>"><img src="<?php print $img_src; ?>" /></a></div>
+    <a href="<?php print $img_url; ?>"><img src="<?php print $img_src; ?>" /></a></div>
 </div>
--- sites/msgr.com/themes/msgr/templates/page.tpl.php
+++ sites/msgr.com/themes/msgr/templates/page.tpl.php
@@ -92,6 +92,12 @@
                        title="<?php print t('Home'); ?>" rel="home"
                        id="logo"><img src="<?php print $logo; ?>"
                                       alt="<?php print t('Home'); ?>"/></a>
+                   <!-- remove mobile logo because main logo now resizes and mobile is producing a second banner -->
+                   <!-- <a href="<?php print $front_page; ?>"
+                       title="<?php print t('Home'); ?>" rel="home"
+                       id="mobile-logo"><img
+                                src="<?php print $base_path; ?><?php print $directory; ?>/logo_mobile.png"
+                                alt="<?php print t('Home'); ?>"/></a> -->
                 <?php endif; ?>
 
                 <?php if ($site_name || $site_slogan): ?>
