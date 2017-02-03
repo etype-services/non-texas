93558716fe60059e318d8a44f14718b09ae9958d
Charlie Llewellin <charlie@llewellin.net>
2017-02-02 20:37:17 -0600

    updates

--- sites/all/themes/news_center/template.php
+++ sites/all/themes/news_center/template.php
@@ -89,6 +89,7 @@ function news_center_preprocess_html(&$variables, $hook) {
         );
     }
 
+    /* add site setting css */
     $nav_color = theme_get_setting('nav_color');
     if (!empty($nav_color)) {
         drupal_add_css(
@@ -115,6 +116,86 @@ function news_center_preprocess_html(&$variables, $hook) {
                 'weight' => '9999',
             )
         );
+    }
+
+    /* add favicons */
+    $icon_path = $base_path . $conf_path .'/files/favicons/';
+
+    $theme_color = array(
+        '#type' => 'html_tag',
+        '#tag' => 'meta',
+        '#attributes' => array(
+            'name' => 'theme-color',
+            'content' => '#ffffff',
+        )
+    );
+    drupal_add_html_head($theme_color, 'theme_color');
+
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $icon_path . 'safari-pinned-tab.svg')) {
+        $mask_icon = array(
+            '#type' => 'html_tag',
+            '#tag' => 'link',
+            '#attributes' => array(
+                'rel' => 'mask-icon',
+                'href' => $icon_path . 'safari-pinned-tab.svg',
+                'color' => '#5bbad5',
+            )
+        );
+        drupal_add_html_head($mask_icon, 'mask_icon');
+
+    }
+
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $icon_path . 'manifest.json')) {
+        $manifest = array(
+            '#type' => 'html_tag',
+            '#tag' => 'link',
+            '#attributes' => array(
+                'rel' => 'manifest',
+                'href' => $icon_path . 'manifest.json',
+            )
+        );
+        drupal_add_html_head($manifest, 'manifest');
+    }
+
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $icon_path . 'favicon-16x16.png')) {
+        $icon16 = array(
+            '#type' => 'html_tag',
+            '#tag' => 'link',
+            '#attributes' => array(
+                'rel' => 'icon',
+                'type' => 'image/png',
+                'sizes' => '16x16',
+                'href' => $icon_path . 'favicon-16x16.png',
+            )
+        );
+        drupal_add_html_head($icon16, 'icon16');
+    }
+
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $icon_path . 'favicon-32x32.png')) {
+        $icon32 = array(
+            '#type' => 'html_tag',
+            '#tag' => 'link',
+            '#attributes' => array(
+                'rel' => 'icon',
+                'type' => 'image/png',
+                'sizes' => '32x32',
+                'href' => $icon_path . 'favicon-32x32.png',
+            )
+        );
+        drupal_add_html_head($icon32, 'icon32');
+    }
+
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $icon_path . 'apple-touch-icon.png')) {
+        $appletouchicon = array(
+            '#type' => 'html_tag',
+            '#tag' => 'link',
+            '#attributes' => array(
+                'rel' => 'apple-touch-icon',
+                'sizes' => '180x180',
+                'href' => $icon_path . 'apple-touch-icon.png',
+            )
+        );
+        drupal_add_html_head($appletouchicon, 'apple-touch-icon');
     }
 
   // The body tag's classes are controlled by the $classes_array variable. To
