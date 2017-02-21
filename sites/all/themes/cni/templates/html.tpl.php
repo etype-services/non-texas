042b16d6af39ad32c6407e00ac876d1b3a4653eb
Charlie Llewellin <charlie@llewellin.net>
2017-02-21 12:56:11 -0600

    cni theme update

--- sites/all/themes/cni/templates/html.tpl.php
+++ sites/all/themes/cni/templates/html.tpl.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="<?php print $language->language; ?>"
       dir="<?php print $language->dir; ?>" <?php print $rdf_namespaces; ?>>
@@ -8,28 +7,27 @@
     <title><?php print $head_title; ?></title>
     <?php print $styles; ?>
     <?php print $scripts; ?>
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet'
-          type='text/css'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href='//fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
+
     <!-- begin code for etype google dfp ads -->
     <script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
-<script>
-  var googletag = googletag || {};
-  googletag.cmd = googletag.cmd || [];
-</script>
+    <script>
+        var googletag = googletag || {};
+        googletag.cmd = googletag.cmd || [];
+    </script>
 
-<script>
-  googletag.cmd.push(function() {
-    googletag.defineSlot('/116205717/eTypeleaderboard', [728, 90], 'div-gpt-ad-1487352373268-0').addService(googletag.pubads());
-    googletag.defineSlot('/116205717/eTypesidebar', [300, 250], 'div-gpt-ad-1487352373268-1').addService(googletag.pubads());
-    googletag.pubads().enableSingleRequest();
-    googletag.enableServices();
-  });
-</script>
+    <script>
+        googletag.cmd.push(function() {
+            googletag.defineSlot('/116205717/eTypeleaderboard', [728, 90], 'div-gpt-ad-1487352373268-0').addService(googletag.pubads());
+            googletag.defineSlot('/116205717/eTypesidebar', [300, 250], 'div-gpt-ad-1487352373268-1').addService(googletag.pubads());
+            googletag.pubads().enableSingleRequest();
+            googletag.enableServices();
+        });
+    </script>
     <!-- end code for etype google dfp ads -->
 </head>
 
@@ -51,45 +49,4 @@
 <?php print $page; ?>
 <?php print $page_bottom; ?>
 </body>
-
 </html>
-=======
-<!DOCTYPE html>
-<html lang="<?php print $language->language; ?>"
-      dir="<?php print $language->dir; ?>" <?php print $rdf_namespaces; ?>>
-
-<head>
-    <?php print $head; ?>
-    <title><?php print $head_title; ?></title>
-    <?php print $styles; ?>
-    <?php print $scripts; ?>
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link href='//fonts.googleapis.com/css?family=Lato' rel='stylesheet'
-          type='text/css'>
-    <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-</head>
-
-<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
-<div id="fb-root"></div>
-<script>(function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-<div id="skip-link">
-    <a href="#main-content"
-       class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-</div>
-<?php print $page_top; ?>
-<?php print $page; ?>
-<?php print $page_bottom; ?>
-</body>
-
-</html>
->>>>>>> 7ccbf999e8cc9c0c5c2ba7eec102c5cc30da5f82
