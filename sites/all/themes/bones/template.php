d30ecb12d28f44c3474db25f7323e0f47b5965fe
Charlie Llewellin <charlie@llewellin.net>
2017-03-26 11:05:14 -0500

    updates to themes

--- sites/all/themes/bones/template.php
+++ sites/all/themes/bones/template.php
@@ -2,22 +2,22 @@
 
 function bones_preprocess_html(&$vars) {
 
-	// Add body class for sidebar layout
+  // Add body class for sidebar layout
   $vars['classes_array'][] = theme_get_setting('sidebar_layout');
 
 }
 
 function bones_preprocess_page(&$vars) {
 
-	$grid_info = get_grid_info();
+  $grid_info = get_grid_info();
 
-	// Create page variables
+  // Create page variables
 
-	$vars['grid_size'] = 'container_' . $grid_info['grid_size'];
-	$vars['grid_full_width'] = 'grid_' . $grid_info['grid_size'];
-	$vars['sidebar_first_grid_width'] = 'grid_' . $grid_info['sidebar_first_width'];
+  $vars['grid_size'] = 'container_' . $grid_info['grid_size'];
+  $vars['grid_full_width'] = 'grid_' . $grid_info['grid_size'];
+  $vars['sidebar_first_grid_width'] = 'grid_' . $grid_info['sidebar_first_width'];
   $vars['sidebar_second_grid_width'] = 'grid_' . $grid_info['sidebar_second_width'];
-	$vars['twitter'] = theme_get_setting('twitter');
+  $vars['twitter'] = theme_get_setting('twitter');
   $vars['facebook'] = theme_get_setting('facebook');
   $vars['pinterest'] = theme_get_setting('pinterest');
 
@@ -40,18 +40,19 @@ function bones_preprocess_page(&$vars) {
 
 function get_grid_info() {
 
-	$grid_info = array();
+  $grid_info = array();
 
-	$grid_info['grid_size'] = theme_get_setting('grid_size');
-	$grid_info['sidebar_first_width'] = theme_get_setting('sidebar_first_width');
-	$grid_info['sidebar_second_width'] = theme_get_setting('sidebar_second_width');
+  $grid_info['grid_size'] = theme_get_setting('grid_size');
+  $grid_info['sidebar_first_width'] = theme_get_setting('sidebar_first_width');
+  $grid_info['sidebar_second_width'] = theme_get_setting('sidebar_second_width');
 
-	for ($region_count = 1; $region_count <= 4; $region_count++) {
-	  $grid_info['preface_' . $region_count . '_grid_width'] = theme_get_setting('preface_' . $region_count . '_grid_width');
-		$grid_info['postscript_' . $region_count . '_grid_width'] = theme_get_setting('postscript_' . $region_count . '_grid_width');
-	}
+  for ($region_count = 1; $region_count <= 4; $region_count++) {
+    $grid_info['preface_' . $region_count . '_grid_width'] = theme_get_setting('preface_' . $region_count . '_grid_width');
+    $grid_info['postscript_' . $region_count . '_grid_width'] = theme_get_setting('postscript_' . $region_count . '_grid_width');
+  }
 
-	return $grid_info;
+  return $grid_info;
+
 }
 
 
