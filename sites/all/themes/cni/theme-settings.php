53c63dd22866da6b58368ad54df83ca9517b5c78
Charlie Llewellin <charlie@llewellin.net>
2017-08-25 11:28:27 -0500

    hartington

--- sites/all/themes/cni/theme-settings.php
+++ sites/all/themes/cni/theme-settings.php
@@ -75,6 +75,13 @@ function cni_form_system_theme_settings_alter(&$form, $form_state) {
     '#default_value' => theme_get_setting('googleplus'),
   );
 
+  $form['advanced_settings']['misc_settings']['youtube'] = array(
+    '#type' => 'textfield',
+    '#title' => t('YouTube'),
+    '#size' => 10,
+    '#default_value' => theme_get_setting('youtube'),
+  );
+
   $form['advanced_settings']['misc_settings']['rssfeed'] = array(
     '#type' => 'textfield',
     '#title' => t('RSS Feed'),
