b73db606a495648f94ff552946d6c438dc961b59
Charlie Llewellin <charlie@llewellin.net>
2017-09-15 16:30:10 -0500

    northeast nebraska

--- sites/northeastnebraskanews.etypegoogle8.com/themes/northeastnebraskanews/templates/page.tpl.php
+++ sites/northeastnebraskanews.etypegoogle8.com/themes/northeastnebraskanews/templates/page.tpl.php
@@ -69,7 +69,17 @@
                               </div>
                           <?php endif; ?>
 
-                            
+                          <?php if ($page['highlighted']): ?>
+                              <div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+                          <?php print render($tabs); ?>
+                          <?php if (!isset($node)): ?>
+                            <?php print render($title_prefix); ?>
+                            <?php if ($title): ?><h1 class="title"
+                                                     id="page-title">
+                                  <span><?php print $title; ?></span>
+                                  </h1><?php endif; ?>
+                            <?php print render($title_suffix); ?>
+                          <?php endif; ?>
 
                           <?php if ($page['preface_1']): ?>
                               <div class="preface-wrapper
@@ -85,17 +95,6 @@
                               </div>
                           <?php endif; ?>
 
-                          <?php if ($page['highlighted']): ?>
-                              <div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-                          <?php print render($tabs); ?>
-                          <?php if (!isset($node)): ?>
-                            <?php print render($title_prefix); ?>
-                            <?php if ($title): ?><h1 class="title"
-                                                     id="page-title">
-                                  <span><?php print $title; ?></span>
-                                  </h1><?php endif; ?>
-                            <?php print render($title_suffix); ?>
-                          <?php endif; ?>
                           <?php print render($page['help']); ?>
                           <?php print render($page['content']); ?>
                           <?php if ($action_links): ?>
