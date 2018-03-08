c3b895f03deb1cda4729eb552d1b9be83a233a57
etypeweb@non-texas.c.modern-photon-112115.internal <etypeweb@non-texas.c.modern-photon-112115.internal>
2018-03-08 09:34:06 -0600

    updates and facebook templates

---
+++ sites/sequoyahcountytimes.etypegoogle8.com/themes/sequoyahcountytimes/templates/views-view-row-rss--facebook-rss-feed--facebook-feed.tpl.php
@@ -0,0 +1,7 @@
+<item>
+    <title><?php print $title; ?></title>
+    <link><?php print $link; ?></link>
+    <image><?php print $img;?></image>
+    <content:encoded><?php print $description;?></content:encoded>
+    <?php print $item_elements; ?>
+</item>
