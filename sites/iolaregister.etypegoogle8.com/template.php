9b6042173ee35608a6040c9826f312657ea15268
Charlie Llewellin <charlie@llewellin.net>
2018-10-17 14:03:26 -0500

    iola register theme

--- sites/iolaregister.etypegoogle8.com/template.php
+++ sites/iolaregister.etypegoogle8.com/template.php
@@ -7,6 +7,7 @@
  */
 
 function iolaregister_preprocess_html() {
+  echo 'yes';
   if ($node = menu_get_object()) {
     $item = entity_load('field_section', array($node->nid));
     print_r($item);
