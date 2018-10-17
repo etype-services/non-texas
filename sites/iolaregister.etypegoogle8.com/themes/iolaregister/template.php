f77d7d29281d07ce78aceda8240283d24c5f148c
Charlie Llewellin <charlie@llewellin.net>
2018-10-17 14:09:18 -0500

    iola register theme

--- sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
+++ sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
@@ -8,7 +8,6 @@
 
 function iolaregister_preprocess_html(&$variables) {
   if ($node = menu_get_object()) {
-    //$item = entity_load('field_section', array($node->nid));
-    //print_r($item);
+    $node_wrapper = entity_metadata_wrapper('node', $node);
   }
 }
