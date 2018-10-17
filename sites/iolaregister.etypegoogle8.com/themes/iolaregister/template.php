c12a8a96f6b5db710db5fca4e5b4483c1108302f
Charlie Llewellin <charlie@llewellin.net>
2018-10-17 15:05:14 -0500

    iola register theme

--- sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
+++ sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
@@ -8,12 +8,16 @@
 
 function iolaregister_preprocess_html(&$variables) {
   if ($node = menu_get_object()) {
-    $node_wrapper = entity_metadata_wrapper('node', $node);
-    $val = $node_wrapper->field_section->value();
-    if ($val[0]->tid == 458) {
-      $variables['mailchimp_js'] = <<<EOT
+    if ($node->type = 'article') {
+      $node_wrapper = entity_metadata_wrapper('node', $node);
+      $val = $node_wrapper->field_section->value();
+      if ($val[0]->tid == 458) {
+        $variables['mailchimp_js'] = <<<EOT
+
 <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"1b2c72e5a787c1eeb66fea624","lid":"e393fc4d97","uniqueMethods":true}) })</script>
+
 EOT;
+      }
     }
   }
 }
