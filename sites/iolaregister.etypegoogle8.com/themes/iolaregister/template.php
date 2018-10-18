d2837084b0fad940659b379525d90e64482f46bc
Charlie Llewellin <charlie@llewellin.net>
2018-10-18 11:56:38 -0500

    drupal 7.6

--- sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
+++ sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
@@ -14,7 +14,7 @@ function iolaregister_preprocess_html(&$variables) {
       if ($val[0]->tid == 458) {
         $variables['mailchimp_js'] = <<<EOT
 
-<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"1b2c72e5a787c1eeb66fea624","lid":"e393fc4d97","uniqueMethods":true}) })</script>
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"1b2c72e5a787c1eeb66fea624","lid":"71b8cfbbd0","uniqueMethods":true}) })</script>
 
 EOT;
       }
