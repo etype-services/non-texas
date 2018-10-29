a050d424142a80cf750585a54cfcd174d96f546d
Charlie Llewellin <charlie@llewellin.net>
2018-10-29 15:18:50 -0500

    iola mailchimp popup

--- sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
+++ sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
@@ -18,6 +18,7 @@ function iolaregister_preprocess_html(&$variables) {
 <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"1b2c72e5a787c1eeb66fea624","lid":"71b8cfbbd0","uniqueMethods":true}) })</script>
 
 EOT;
+          break;
         }
       }
     }
