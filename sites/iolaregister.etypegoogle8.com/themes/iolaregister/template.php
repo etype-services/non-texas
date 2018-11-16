b13714b1ed40f1445cb04aa9dc037498900a09f2
Charlie Llewellin <charlie@llewellin.net>
2018-11-15 18:36:51 -0600

    iola mailchimp

--- sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
+++ sites/iolaregister.etypegoogle8.com/themes/iolaregister/template.php
@@ -12,7 +12,7 @@ function iolaregister_preprocess_html(&$variables) {
       $node_wrapper = entity_metadata_wrapper('node', $node);
       $val = $node_wrapper->field_section->value();
       foreach ($val as $item) {
-        if ($item->tid == 468) { // local news section
+        if ($item->tid == 463) { // obituaries
           $variables['mailchimp_js'] = <<<EOT
 
 <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"1b2c72e5a787c1eeb66fea624","lid":"71b8cfbbd0","uniqueMethods":true}) })</script>
