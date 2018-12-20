edc2cca31a5e694fcfdc0ea3197e0fa549edf4db
etypeweb@luther <etypeweb@luther>
2018-12-20 08:57:40 -0600

    updates

--- sites/sites.php
+++ sites/sites.php
@@ -347,3 +347,235 @@ $sites['www.columbusnews-report.com'] = 'columbusnews-report.etypegoogle9.com';
 /* dovecreekpress.etypegoogle7.com */
 $sites['dovecreekpress.etypegoogle9.com'] = 'dovecreekpress.etypegoogle9.com';
 $sites['www.dovecreekpress.com'] = 'dovecreekpress.etypegoogle9.com';
+
+$sites['chamberlainsun.etypegoogle9.com'] = 'chamberlainsun.etypegoogle9.com';
+$sites['www.chamberlainsun.com'] = 'chamberlainsun.etypegoogle9.com';
+
+$sites['southreporter.etypegoogle9.com'] = 'southreporter.etypegoogle9.com';
+$sites['southreporter.com'] = 'southreporter.etypegoogle9.com';
+$sites['www.southreporter.com'] = 'southreporter.etypegoogle9.com';
+
+$sites['ainsworthnews.etypegoogle9.com'] = 'ainsworthnews.etypegoogle9.com';
+$sites['ainsworthnews.com'] = 'ainsworthnews.etypegoogle9.com';
+$sites['www.ainsworthnews.com'] = 'ainsworthnews.etypegoogle9.com';
+$sites['valentinenews.com'] = 'ainsworthnews.etypegoogle9.com';
+$sites['www.valentinenews.com'] = 'ainsworthnews.etypegoogle9.com';
+$sites['gregorynews.com'] = 'ainsworthnews.etypegoogle9.com';
+$sites['www.gregorynews.com'] = 'ainsworthnews.etypegoogle9.com';
+
+$sites['deweycountyrecord.etypegoogle9.com'] = 'deweycountyrecord.etypegoogle9.com';
+$sites['deweycountyrecord.com'] = 'deweycountyrecord.etypegoogle9.com';
+$sites['www.deweycountyrecord.com'] = 'deweycountyrecord.etypegoogle9.com';
+
+$sites['okeenerecord.etypegoogle9.com'] = 'okeenerecord.etypegoogle9.com';
+$sites['okeenerecord.com'] = 'okeenerecord.etypegoogle9.com';
+$sites['www.okeenerecord.com'] = 'okeenerecord.etypegoogle9.com';
+
+$sites['columbusnews-report.etypegoogle9.com'] = 'columbusnews-report.etypegoogle9.com';
+$sites['www.columbusnews-report.com'] = 'archive.columbusnews-report.etypegoogle9.com';
+
+$sites['lincolnsentinel.etypegoogle9.com'] = 'lincolnsentinel.etypegoogle9.com';
+$sites['lincolnsentinel.com'] = 'lincolnsentinel.etypegoogle9.com';
+$sites['www.lincolnsentinel.com'] = 'lincolnsentinel.etypegoogle9.com';
+
+$sites['dickinsonnewstimes.etypegoogle9.com'] = 'dickinsonnewstimes.etypegoogle9.com';
+$sites['www.dickinsonnewstimes.com'] = 'dickinsonnewstimes.etypegoogle9.com';
+
+$sites['anewspaper.etypegoogle9.com'] = 'anewspaper.etypegoogle9.com';
+$sites['anewspaper.net'] = 'anewspaper.etypegoogle9.com';
+$sites['www.anewspaper.net'] = 'anewspaper.etypegoogle9.com';
+$sites['nemahacountyherald.com'] = 'anewspaper.etypegoogle9.com';
+$sites['www.nemahacountyherald.com'] = 'anewspaper.etypegoogle9.com';
+
+$sites['claycountyliving.etypegoogle9.com'] = 'claycountyliving.etypegoogle9.com';
+$sites['claycountyliving.com'] = 'claycountyliving.etypegoogle9.com';
+$sites['www.claycountyliving.com'] = 'claycountyliving.etypegoogle9.com';
+
+$sites['thehonker.etypegoogle9.com'] = 'thehonker.etypegoogle9.com';
+$sites['thehonker.com'] = 'thehonker.etypegoogle9.com';
+$sites['www.thehonker.com'] = 'thehonker.etypegoogle9.com';
+
+$sites['bannerpresspaper.etypegoogle9.com'] = 'bannerpresspaper.etypegoogle9.com';
+$sites['bannerpresspaper.com'] = 'bannerpresspaper.etypegoogle9.com';
+$sites['www.bannerpresspaper.com'] = 'bannerpresspaper.etypegoogle9.com';
+
+$sites['covebanner.etypegoogle9.com'] = 'covebanner.etypegoogle9.com';
+$sites['covebanner.com'] = 'covebanner.etypegoogle9.com';
+$sites['www.covebanner.com'] = 'covebanner.etypegoogle9.com';
+
+$sites['starheraldnews.etypegoogle9.com'] = 'starheraldnews.etypegoogle9.com';
+$sites['starheraldnews.com'] = 'starheraldnews.etypegoogle9.com';
+$sites['www.starheraldnews.com'] = 'starheraldnews.etypegoogle9.com';
+
+$sites['canton-times.etypegoogle9.com'] = 'canton-times.etypegoogle9.com';
+$sites['canton-times.com'] = 'canton-times.etypegoogle9.com';
+$sites['www.canton-times.com'] = 'canton-times.etypegoogle9.com';
+
+$sites['eagleprint.etypegoogle9.com'] = 'eagleprint.etypegoogle9.com';
+$sites['eagleprint.net'] = 'eagleprint.etypegoogle9.com';
+$sites['www.eagleprint.net'] = 'eagleprint.etypegoogle9.com';
+
+$sites['gspublishing.etypegoogle9.com'] = 'gspublishing.etypegoogle9.com';
+$sites['gspublishing.net'] = 'gspublishing.etypegoogle9.com';
+$sites['www.gspublishing.net'] = 'gspublishing.etypegoogle9.com';
+
+$sites['sulphurtimes.etypegoogle9.com'] = 'sulphurtimes.etypegoogle9.com';
+$sites['sulphurtimes.com'] = 'sulphurtimes.etypegoogle9.com';
+$sites['www.sulphurtimes.com'] = 'sulphurtimes.etypegoogle9.com';
+
+$sites['imperialrepublican.etypegoogle9.com'] = 'imperialrepublican.etypegoogle9.com';
+$sites['imperialrepublican.com'] = 'imperialrepublican.etypegoogle9.com';
+$sites['www.imperialrepublican.com'] = 'imperialrepublican.etypegoogle9.com';
+
+$sites['holyokeenterprise.etypegoogle9.com'] = 'holyokeenterprise.etypegoogle9.com';
+$sites['holyokeenterprise.com'] = 'holyokeenterprise.etypegoogle9.com';
+$sites['www.holyokeenterprise.com'] = 'holyokeenterprise.etypegoogle9.com';
+
+$sites['granttribune.etypegoogle9.com'] = 'granttribune.etypegoogle9.com';
+$sites['granttribune.com'] = 'granttribune.etypegoogle9.com';
+$sites['www.granttribune.com'] = 'granttribune.etypegoogle9.com';
+
+$sites['waunetabreeze.etypegoogle9.com'] = 'waunetabreeze.etypegoogle9.com';
+$sites['waunetabreeze.com'] = 'waunetabreeze.etypegoogle9.com';
+$sites['www.waunetabreeze.com'] = 'waunetabreeze.etypegoogle9.com';
+$sites['waunetanebraska.com'] = 'waunetabreeze.etypegoogle9.com';
+$sites['www.waunetanebraska.com'] = 'waunetabreeze.etypegoogle9.com';
+
+$sites['southarkansassun.etypegoogle9.com'] = 'southarkansassun.etypegoogle9.com';
+$sites['southarkansassun.com'] = 'southarkansassun.etypegoogle9.com';
+$sites['www.southarkansassun.com'] = 'southarkansassun.etypegoogle9.com';
+$sites['southarkansassun.net'] = 'southarkansassun.etypegoogle9.com';
+$sites['www.southarkansassun.net'] = 'southarkansassun.etypegoogle9.com';
+
+$sites['humphreydemocrat.etypegoogle9.com'] = 'humphreydemocrat.etypegoogle9.com';
+$sites['humphreydemocrat.com'] = 'humphreydemocrat.etypegoogle9.com';
+$sites['www.humphreydemocrat.com'] = 'humphreydemocrat.etypegoogle9.com';
+
+$sites['rileycountian.etypegoogle9.com'] = 'rileycountian.etypegoogle9.com';
+$sites['rileycountian.com'] = 'rileycountian.etypegoogle9.com';
+$sites['www.rileycountian.com'] = 'rileycountian.etypegoogle9.com';
+
+$sites['councilgroverepublican.etypegoogle9.com'] = 'councilgroverepublican.etypegoogle9.com';
+$sites['councilgroverepublican.com'] = 'councilgroverepublican.etypegoogle9.com';
+$sites['www.councilgroverepublican.com'] = 'councilgroverepublican.etypegoogle9.com';
+
+$sites['perrysburg.etypegoogle9.com'] = 'perrysburg.etypegoogle9.com';
+$sites['perrysburg.com'] = 'perrysburg.etypegoogle9.com';
+$sites['www.perrysburg.com'] = 'perrysburg.etypegoogle9.com';
+$sites['rossford.com'] = 'perrysburg.etypegoogle9.com';
+$sites['www.rossford.com'] = 'perrysburg.etypegoogle9.com';
+$sites['pointandshoreland.com'] = 'perrysburg.etypegoogle9.com';
+$sites['www.pointandshoreland.com'] = 'perrysburg.etypegoogle9.com';
+$sites['hollandsfj.us'] = 'perrysburg.etypegoogle9.com';
+$sites['www.hollandsfj.us'] = 'perrysburg.etypegoogle9.com';
+$sites['welchpublishing.com'] = 'perrysburg.etypegoogle9.com';
+$sites['www.welchpublishing.com'] = 'perrysburg.etypegoogle9.com';
+
+$sites['eurekaherald.etypegoogle9.com'] = 'eurekaherald.etypegoogle9.com';
+$sites['eurekaherald.com'] = 'eurekaherald.etypegoogle9.com';
+$sites['www.eurekaherald.com'] = 'eurekaherald.etypegoogle9.com';
+
+$sites['beckhamcountyrecord.etypegoogle9.com'] = 'beckhamcountyrecord.etypegoogle9.com';
+$sites['beckhamcountyrecord.com'] = 'beckhamcountyrecord.etypegoogle9.com';
+$sites['www.beckhamcountyrecord.com'] = 'beckhamcountyrecord.etypegoogle9.com';
+$sites['sayrerecord.com'] = 'beckhamcountyrecord.etypegoogle9.com';
+$sites['www.sayrerecord.com'] = 'beckhamcountyrecord.etypegoogle9.com';
+$sites['spitzerpublishing.com'] = 'beckhamcountyrecord.etypegoogle9.com';
+$sites['www.spitzerpublishing.com'] = 'beckhamcountyrecord.etypegoogle9.com';
+
+$sites['valleynewsandviews.etypegoogle9.com'] = 'valleynewsandviews.etypegoogle9.com';
+$sites['valleynewsandviews.com'] = 'valleynewsandviews.etypegoogle9.com';
+$sites['www.valleynewsandviews.com'] = 'valleynewsandviews.etypegoogle9.com';
+
+$sites['yourxgroup.etypegoogle9.com'] = 'yourxgroup.etypegoogle9.com';
+$sites['yourxgroup.com'] = 'yourxgroup.etypegoogle9.com';
+$sites['www.yourxgroup.com'] = 'yourxgroup.etypegoogle9.com';
+
+$sites['oologahonline.etypegoogle9.com'] = 'oologahonline.etypegoogle9.com';
+$sites['oologahonline.com'] = 'oologahonline.etypegoogle9.com';
+$sites['www.oologahonline.com'] = 'oologahonline.etypegoogle9.com';
+
+$sites['wilmingtonmatters.etypegoogle9.com'] = 'wilmingtonmatters.etypegoogle9.com';
+$sites['wilmingtonmatters.com'] = 'wilmingtonmatters.etypegoogle9.com';
+$sites['www.wilmingtonmatters.com'] = 'wilmingtonmatters.etypegoogle9.com';
+$sites['chestermatters.news'] = 'wilmingtonmatters.etypegoogle9.com';
+$sites['www.chestermatters.news'] = 'wilmingtonmatters.etypegoogle9.com';
+
+$sites['clipperpubco.etypegoogle9.com'] = 'clipperpubco.etypegoogle9.com';
+$sites['clipperpubco.com'] = 'clipperpubco.etypegoogle9.com';
+$sites['www.clipperpubco.com'] = 'clipperpubco.etypegoogle9.com';
+
+$sites['lexingtonchronicle.etypegoogle9.com'] = 'lexingtonchronicle.etypegoogle9.com';
+$sites['lexingtonchronicle.com'] = 'lexingtonchronicle.etypegoogle9.com';
+$sites['www.lexingtonchronicle.com'] = 'lexingtonchronicle.etypegoogle9.com';
+
+$sites['belleplainenews.etypegoogle9.com'] = 'belleplainenews.etypegoogle9.com';
+$sites['belleplainenews.com'] = 'belleplainenews.etypegoogle9.com';
+$sites['www.belleplainenews.com'] = 'belleplainenews.etypegoogle9.com';
+
+$sites['auroranewsregister.etypegoogle9.com'] = 'auroranewsregister.etypegoogle9.com';
+$sites['auroranewsregister.com'] = 'auroranewsregister.etypegoogle9.com';
+$sites['www.auroranewsregister.com'] = 'auroranewsregister.etypegoogle9.com';
+
+$sites['stjamesparishtoday.etypegoogle9.com'] = 'stjamesparishtoday.etypegoogle9.com';
+$sites['stjamesparishtoday.com'] = 'stjamesparishtoday.etypegoogle9.com';
+$sites['www.stjamesparishtoday.com'] = 'stjamesparishtoday.etypegoogle9.com';
+$sites['lutchernews.com'] = 'stjamesparishtoday.etypegoogle9.com';
+$sites['www.lutchernews.com'] = 'stjamesparishtoday.etypegoogle9.com';
+
+$sites['seoksports.etypegoogle9.com'] = 'seoksports.etypegoogle9.com';
+$sites['seoksports.com'] = 'seoksports.etypegoogle9.com';
+$sites['www.seoksports.com'] = 'seoksports.etypegoogle9.com';
+
+$sites['drippingspringsnews.etypegoogle9.com'] = 'drippingspringsnews.etypegoogle9.com';
+$sites['drippingspringsnews.com'] = 'drippingspringsnews.etypegoogle9.com';
+$sites['www.drippingspringsnews.com'] = 'drippingspringsnews.etypegoogle9.com';
+
+$sites['wimberleyview.etypegoogle9.com'] = 'wimberleyview.etypegoogle9.com';
+$sites['wimberleyview.com'] = 'wimberleyview.etypegoogle9.com';
+$sites['www.wimberleyview.com'] = 'wimberleyview.etypegoogle9.com';
+
+$sites['steelcountrybee.etypegoogle9.com'] = 'steelcountrybee.etypegoogle9.com';
+$sites['steelcountrybee.com'] = 'steelcountrybee.etypegoogle9.com';
+$sites['www.steelcountrybee.com'] = 'steelcountrybee.etypegoogle9.com';
+
+$sites['campcountynow.etypegoogle9.com'] = 'campcountynow.etypegoogle9.com';
+$sites['campcountynow.com'] = 'campcountynow.etypegoogle9.com';
+$sites['www.campcountynow.com'] = 'campcountynow.etypegoogle9.com';
+
+$sites['gothenburgtimes.etypegoogle9.com'] = 'gothenburgtimes.etypegoogle9.com';
+$sites['gothenburgtimes.com'] = 'gothenburgtimes.etypegoogle9.com';
+$sites['www.gothenburgtimes.com'] = 'gothenburgtimes.etypegoogle9.com';
+
+$sites['yourokmulgee.etypegoogle9.com'] = 'yourokmulgee.etypegoogle9.com';
+$sites['yourokmulgee.com'] = 'yourokmulgee.etypegoogle9.com';
+$sites['www.yourokmulgee.com'] = 'yourokmulgee.etypegoogle9.com';
+
+$sites['henryettafree-lance.etypegoogle9.com'] = 'henryettafree-lance.etypegoogle9.com';
+$sites['henryettafree-lance.com'] = 'henryettafree-lance.etypegoogle9.com';
+$sites['www.henryettafree-lance.com'] = 'henryettafree-lance.etypegoogle9.com';
+
+/* bladeempire.etypegoogle9.com */
+$sites['www.bladeempire.com'] = 'bladeempire.etypegoogle9.com';
+
+/* easterntimesregister.etypegoogle9.com */
+$sites['www.easterntimesregister.com'] = 'easterntimesregister.etypegoogle9.com';
+
+/* eufaulaindianjournal.etypegoogle9.com */
+$sites['www.eufaulaindianjournal.com'] = 'eufaulaindianjournal.etypegoogle9.com';
+
+/* mcintoshdemocrat.etypegoogle9.com */
+$sites['www.mcintoshdemocrat.com'] = 'mcintoshdemocrat.etypegoogle9.com';
+
+/* viannews.etypegoogle9.com */
+$sites['www.viannews.com'] = 'viannews.etypegoogle9.com';
+
+/* beloitcall.etypegoogle9.com */
+$sites['beloitcall.etypegoogle9.com'] = 'beloitcall.etypegoogle9.com';
+$sites['www.beloitcall.com'] = 'beloitcall.etypegoogle9.com';
+
+/* newcastlepacer.etypegoogle9.com */
+$sites['www.newcastlepacer.com'] = 'newcastlepacer.etypegoogle9.com';
+
+/* heringtontimes.etypegoogle9.com */
+$sites['www.heringtontimes.com'] = 'heringtontimes.etypegoogle9.com';
