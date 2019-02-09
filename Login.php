58671cb832ba467c84462caac6ac5638344ce777
root <root@luther.c.modern-photon-112115.internal>
2019-02-08 19:22:55 -0600

    remove sports.php etc

--- Login.php
+++
@@ -1,3 +0,0 @@
-<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-require_once('sites/clintondailynews.com/inc/login.inc');
-?>
