a12ce374fb590f9d3fceaebeaff71ec53d58c967
etypeweb@luther <etypeweb@luther>
2019-04-05 18:11:59 -0500

    automatic updates

--- sites/sites.php
+++ sites/sites.php
@@ -102,9 +102,9 @@ $sites['www.pawneerepublican.com'] = 'pawneerepublican.com';
 $sites['pawneenews.com'] = 'pawneerepublican.com';
 $sites['www.pawneenews.com'] = 'pawneerepublican.com';
 
-$sites['indyrepnews.etypegoogle8.com'] = 'indyrepnews.com';
-$sites['indyrepnews.com'] = 'indyrepnews.com';
-$sites['www.indyrepnews.com'] = 'indyrepnews.com';
+$sites['indyrepnews.etypegoogle8.com'] = 'archive.indyrepnews.com';
+$sites['indyrepnews.com'] = 'archive.indyrepnews.com';
+$sites['www.indyrepnews.com'] = 'archive.indyrepnews.com';
 
 $sites['archive.msgr.com'] = 'archive.msgr.etypegoogle8.com';
 
