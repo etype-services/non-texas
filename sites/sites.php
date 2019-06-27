238f3a4f89a2b7dad4022e1e1285034a172dda36
etypeweb@luther <etypeweb@luther>
2019-06-27 14:11:42 -0500

    automatic updates

--- sites/sites.php
+++ sites/sites.php
@@ -1,6 +1,7 @@
 <?php
 /* test.etypegoogle7.com */
 $sites['test.eunicetoday.com'] = 'test.etypegoogle7.com';
+$sites['www.etypetest.com'] = 'test.etypegoogle7.com';
 
 /* eunice today archive */
 $sites['archive.eunicetoday.com'] = 'eunicearchive.etypegoogle7.com';
