79606e55d0a7515fce0e54169225bee4e1d6fe0c
Charlie Llewellin <charlie@llewellin.net>
2020-05-21 12:41:44 -0500

    adding pico module

--- sites/all/modules/etype_pico/js/etype_pico.js
+++ sites/all/modules/etype_pico/js/etype_pico.js
@@ -3,8 +3,12 @@
   'use strict';
   Drupal.behaviors.etypePico = {
     attach: function (context, settings) {
-      $('#user-menu a[href="/pico-login"]').addClass('PicoRule PicoSignal');
-      $('#user-menu a[href="/pico=subscribe"]').addClass('PicoPlan PicoSignal');
+      $('#user-menu a[href="/pico-login"]').addClass('PicoRule PicoSignal').click(function(e) {
+        e.preventDefault();
+      });
+      $('#user-menu a[href="/pico=subscribe"]').addClass('PicoPlan PicoSignal').click(function(e) {
+        e.preventDefault();
+      });
     }
   };
 })(jQuery);
